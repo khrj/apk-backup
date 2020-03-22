d04b882094f0ba327be3d33243fb4296a4e4eb73
Khushraj Rathod <khushrajrathod@gmail.com>
2020-03-22 19:51:56 +0530

    Fix extract-all.sh

--- Tools/extract-all.sh
+++ Tools/extract-all.sh
@@ -3,5 +3,6 @@ scriptdir=$(cd ./$(dirname $0)/; pwd)
 apkdir=$(cd $scriptdir/../Apks/; pwd) # APK DIR, Absolute
 echo $apkdir
 
+adb push $apkdir/.gitignore /sdcard/ExtractedApks/
 $scriptdir/adb-sync --reverse --delete /sdcard/ExtractedApks/ $apkdir
-find $apkdir -name "* *" -type f | rename 's/ /_/g' # Replace spaces with _s
+find $apkdir -name "* *" -type f | rename 's/ /_/g' # Replace spaces with _
