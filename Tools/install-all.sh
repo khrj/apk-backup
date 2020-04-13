b50b4780efa3625313cc911405aea5ee2e7a7ee1
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-13 17:16:05 +0530

    Polish install-all.sh

--- Tools/install-all.sh
+++ Tools/install-all.sh
@@ -1,16 +1,21 @@
-scriptdir=$(cd ./$(dirname $0)/; pwd)
-apkdir=$(cd $scriptdir/../Apks/; pwd)/ # APK DIR, Absolute
-echo THE APKS ARE STORED IN
-echo $apkdir
+#!/bin/bash
 
-cd $apkdir
-for folder in $(ls)
+set -e
+
+scriptdir=$(cd ./"$(dirname "$0")"/; pwd)
+apkdir=$(cd "$scriptdir"/../Apks/; pwd)/ # APK DIR, Absolute
+echo THE APKS ARE STORED IN
+echo "$apkdir"
+
+cd "$apkdir"
+for folder in *
 do
-    echo $folder
-    cd $folder
-    if [[ $(ls -1 | wc -l | awk '{print $1}') != '1' ]]
+    [[ -e "${folder}" ]] || break
+    echo "$folder"
+    cd "$folder"
+    if [[ $(find . | wc -l | awk '{print $1}') != '1' ]]
     then
-        adb install-multiple ${$(ls | tr "\n" " ")//version/}
+        adb install-multiple ${$(find . | tr "\n" " ")//version/}
     else
         adb install base.apk
     fi
