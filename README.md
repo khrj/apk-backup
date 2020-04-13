beffcecc02b53bd026ce7aa1122761e0f1c437cf
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-13 12:36:04 +0530

    Improve extractor script to support directories with spaces, update README.md

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
 # ApkBackup
 Lets you backup Apks from your phone and restore them later
 
-Note: Does not backup app data
-
 # Usage
 
 ## Backup
 
-Note: Apps must be on internal storage and not on sdcard
-
 Run `Tools/extract-all.sh`
-
-### Features
-1. Automatically checks if app has been backup up before, and whether the app has been updated since the backup
-2. Completely automatic backup, without any dependencies.
-3. Does not require root.
 
 ## Restore
 Run `Tools/install-all.sh`
@@ -23,3 +14,14 @@ Run `Tools/install-all.sh`
 **CAUTION**: Uninstalls all installed user apps from android phone
 
 Run `Tools/uninstall-all.sh`
+
+# Features
+1. Automatically checks if app has been backup up before, and whether the app has been updated since the backup
+2. Completely automatic backup, without any dependencies.
+3. Does not require root.
+4. Supports Split apks
+5. Does not require any configuration, and doesn't have any options
+
+# Caveats
+1. Apps cannot be on external sdcard
+2. Does not backup app data
--- Tools/extract-all.sh
+++ Tools/extract-all.sh
@@ -1,46 +1,49 @@
+#!/bin/bash
+
+set -e
+
 # Color coding
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 YELLOW='\033[1;33m'
-RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Start of script
-scriptdir=$(cd ./$(dirname $0)/; pwd)
-apkdir=$(cd $scriptdir/../Apks/; pwd)/ # APK DIR, Absolute
+scriptdir=$(cd ./"$(dirname "${0}")"/; pwd)
+apkdir=$(cd "${scriptdir}"/../Apks/; pwd)/ # APK DIR, Absolute
 
 printf "${YELLOW}THE APKS ARE STORED IN\n"
-printf "$apkdir\n${NC}"
+printf "${apkdir}\n${NC}"
 
-cd "$apkdir"
+cd "${apkdir}"
 for package in $(adb shell "cmd package list packages -3" | cut -d ":" -f 2)
 do
-    apks=$(adb shell "cmd package path $package" | cut -d ":" -f 2)
-    folder=$(echo $apks | tr '\n' ' ' | cut -d ' ' -f 1 | rev | cut -d '/' -f 2 | rev | cut -d '-' -f 1)
+    apks=$(adb shell "cmd package path ${package}" | cut -d ":" -f 2)
+    folder=$(echo "${apks}" | tr '\n' ' ' | cut -d ' ' -f 1 | rev | cut -d '/' -f 2 | rev | cut -d '-' -f 1)
     printf "\n"
-    if [[ ! -d "$apkdir"/$folder ]] # If the apk hasn't been previously backed up
+    if [[ ! -d "${apkdir}"/${folder} ]] # If the apk hasn't been previously backed up
     then
-        printf "$folder has not been backed up before\n"
-        mkdir $folder
-        cd "$apkdir"/$folder
-        for apk in $apks
+        printf "${folder} has not been backed up before\n"
+        mkdir "${folder}"
+        cd "${apkdir}"/"${folder}"
+        for apk in ${apks}
         do
-            adb pull $apk
+            adb pull "${apk}"
         done
-        cd "$apkdir"
+        cd "${apkdir}"
     else
-        printf "$folder has been previously backed up\n"
-        cd "$apkdir"/$folder
-        for apk in $apks
+        printf "${folder} has been previously backed up\n"
+        cd "${apkdir}"/"${folder}"
+        for apk in ${apks}
         do
-            apklocal=$(echo $apk | rev | cut -d '/' -f 1 | rev)
-            if [[ $(adb shell sha256sum $apk | cut -d ' ' -f 1) != $(sha256sum $apklocal | cut -d ' ' -f 1) ]]
+            apklocal=$(echo "${apk}" | rev | cut -d '/' -f 1 | rev)
+            if [[ $(adb shell sha256sum "${apk}" | cut -d ' ' -f 1) != $(sha256sum "${apklocal}" | cut -d ' ' -f 1) ]]
             then
-                printf "${ORANGE}$apk has been updated since last backup\n${NC}, backing up again"
-                rm "$apkdir"/$folder/$apklocal
-                adb pull $apk
+                printf "${ORANGE}${apk} has been updated since last backup\n${NC}, backing up again"
+                rm "${apkdir}"/"${folder}"/"${apklocal}"
+                adb pull "${apk}"
             else
-                printf "${GREEN}$apk has not changed since last backup\n${NC}"
+                printf "${GREEN}${apk} has not changed since last backup\n${NC}"
             fi
         done
         cd "$apkdir"
