60d4405a6c22887b11e29c0392519f100f848047
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-13 17:18:16 +0530

    Polish uninstall-all.sh

--- Tools/uninstall-all.sh
+++ Tools/uninstall-all.sh
@@ -1,10 +1,12 @@
-read -p "Are you sure you want to uninstall all user apps from your phone (y/n)?" choice
+#!/bin/bash
+
+read -p -r "Are you sure you want to uninstall all user apps from your phone (y/n)?" choice
 case "$choice" in
   y|Y )
     for package in $(adb shell cmd package list packages -3 | cut -d ":" -f 2)
     do
-        echo $package
-        adb uninstall $package
+        echo "$package"
+        adb uninstall "$package"
     done
     ;;
   n|N )
