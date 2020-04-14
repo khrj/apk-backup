3fefe1a440d00cbd9b924963637b2be1bccd0956
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-15 00:17:59 +0530

    Cleanup uninstaller script

--- Tools/uninstall-all.sh
+++ Tools/uninstall-all.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-read -p -r "Are you sure you want to uninstall all user apps from your phone (y/n)?" choice
-case "$choice" in
-  y|Y )
+read -p "Are you sure you want to uninstall all user apps from your phone (y/n)?" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
     for package in $(adb shell cmd package list packages -3 | cut -d ":" -f 2)
     do
         echo "$package"
         adb uninstall "$package"
     done
-    ;;
-  n|N )
-    exit 1
-    ;;
-  * )
-    exit 1
-    ;;
-esac
+
+fi
