4da0c924ed0904e2eb99c1f3f3c1f71030599ff0
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-30 22:24:12 +0530

    Add gpl notice

--- Tools/extract-all.sh
+++ Tools/extract-all.sh
@@ -1,3 +1,20 @@
+# Copyright 2020 Khushraj Rathod
+#
+# This file is part of ApkBackup.
+#
+# ApkBackup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ApkBackup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ApkBackup.  If not, see <https://www.gnu.org/licenses/>.
+
 #!/bin/bash
 
 set -e
@@ -41,12 +58,12 @@ do
         if [[ ${version} != $(cat ./version) ]]
         then
             printf "${ORANGE}%s has been updated since last backup, backing up again\n${NC}" "${package}"
-            
+
             cd ..
             rm -r ./"${package}"
             mkdir "${package}"
             cd "${package}"
-            
+
             echo "${version}" > ./version
             for apk in ${apks}
             do
@@ -76,4 +93,3 @@ done
 printf "%s\n" "${REMOVED_PACKAGES[@]}"
 # shellcheck disable=SC2059
 printf "\n${YELLOW}SUCCESS!${NC}\n"
-
